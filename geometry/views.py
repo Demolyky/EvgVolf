20b0db8ae20be007caf1d0733939880690227f69
Mikhail Volf <misha.volf93@gmail.com>
2022-03-23 22:42:20 +0300

    add square and circle

--- geometry/views.py
+++ geometry/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 def get_calculate_geometry(request, calculate_geometry):
-
+    return HttpResponse('ggwp')
